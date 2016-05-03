8197c228e0c845c32cfa6c61036335af9d657a55
Eamonn de Leastar <edeleastar@wit.ie>
2016-05-03 08:32:12 +0100

    simple markdown parsing introduced

--- tutor.js
+++ tutor.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const glob = require('glob-promise');
+const generateLab = require('./lab');
+
+glob('course.md').then(files => {
+  if (files.length == 1) {
+    console.log('Publishing course');
+  } else {
+    glob('topic.md').then(files => {
+      if (files.length == 1) {
+        console.log('Publishing topic');
+      } else {
+        console.log('Publishing lab');
+        const lab = generateLab();
+        console.log(lab);
+      }
+    });
+  }
+});
+
