096341931628a7372788a9f58c1f1a49c3ef7c21
Eamonn de Leastar <edeleastar@wit.ie>
2016-12-07 09:32:19 +0000

    introduce new command to create starter project

--- tutor.js
+++ tutor.js
@@ -5,6 +5,7 @@
 const version = require('./package.json').version;
 
 const fs = require('fs');
+var sh = require('shelljs');
 const glob = require('glob');
 const futils = require('./utils/futils');
 const Lab = require('./model/lab').Lab;
@@ -21,25 +22,37 @@ nunjucks.installJinjaCompat();
 program.arguments('<file>')
     .version(version)
     .option('-p, --private', 'Generate full private site')
+    .option('-n, --new', 'Create a template course')
     .parse(process.argv);
 
-console.log('tutors course web generator: ' + version);
-if (fs.existsSync('portfolio.yaml')) {
-  const portfolio = new Portfolio('portfolio');
-  portfolio.publish('public-site');
-} else if (fs.existsSync('course.md')) {
-  const course = new Course('course');
-  course.publish('public-site');
-  if (program.private) {
-    const privateCourse = new Course('course', true);
-    privateCourse.publish('private-site');
+if (program.new) {
+  console.log('Creating new template course...');
+  if (sh.exec('git clone https://github.com/edeleastar/tutors-starter.git', { silent: false }).code !== 0) {
+    console.log('fix this and try again?');
+  } else {
+    console.log('Next steps...');
+    console.log('Cd into "tutors-starter" and run "tutors" again');
+    console.log('This will generate the course web in "tutors-starter/public-site"');
   }
-} else if (fs.existsSync('topic.md')) {
-  const topic = new Topic('topic');
-  topic.publish('public-site');
-} else if (glob.sync('*.md').length > 0) {
-  const lab = new Lab();
-  lab.publish('../public-site/' + futils.getParentFolder());
 } else {
-  console.log('Unable to detect lab, topic or course');
+  console.log('tutors course web generator: ' + version);
+  if (fs.existsSync('portfolio.yaml')) {
+    const portfolio = new Portfolio('portfolio');
+    portfolio.publish('public-site');
+  } else if (fs.existsSync('course.md')) {
+    const course = new Course('course');
+    course.publish('public-site');
+    if (program.private) {
+      const privateCourse = new Course('course', true);
+      privateCourse.publish('private-site');
+    }
+  } else if (fs.existsSync('topic.md')) {
+    const topic = new Topic('topic');
+    topic.publish('public-site');
+  } else if (glob.sync('*.md').length > 0) {
+    const lab = new Lab();
+    lab.publish('../public-site/' + futils.getParentFolder());
+  } else {
+    console.log('Unable to detect lab, topic or course');
+  }
 }
